dfbd974e24531f54de4924e04e85e79a61b3999c
Dave Carr <davecarr1024@gmail.com>
2021-12-25 20:11:20 -0500

    lexer

--- core/lexer.py
+++ core/lexer.py
@@ -1,77 +1,119 @@
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from functools import cached_property
-from typing import MutableSequence, Optional, Sequence
+from typing import Sequence
+
+from . import processor
 
 
 @dataclass(frozen=True)
-class Lexer:
-    rules: Sequence['Lexer.Rule']
+class ResultValue(processor.ResultValue):
+    value: str
 
-    @dataclass(frozen=True)
-    class Error(Exception):
-        state: 'Lexer.State'
 
-    @dataclass(frozen=True)
-    class AmbiguityError(Error):
-        results: Sequence['Lexer.Rule.Result']
+@dataclass(frozen=True)
+class State(processor.State):
+    input: str
+    pos: int = 0
 
-    @dataclass(frozen=True)
-    class State:
-        input: str
-        pos: int = 0
+    @property
+    def empty(self) -> bool:
+        return self.pos >= len(self.input)
 
-        @cached_property
-        def empty(self) -> bool:
-            return self.pos > len(self.input)
+    @cached_property
+    def value(self) -> str:
+        return self.input[self.pos:]
 
-        @cached_property
-        def value(self) -> str:
-            if self.empty:
-                raise Lexer.Error(self)
-            return self.input[self.pos:]
+    def with_pos(self, pos: int) -> 'State':
+        return State(self.input, pos)
 
-    @dataclass(frozen=True)
-    class Rule(ABC):
-        name: str
+    def after(self, result_value: ResultValue) -> 'State':
+        if not self.value.startswith(result_value.value):
+            raise ValueError(result_value)
+        return self.with_pos(self.pos+len(result_value.value))
 
-        @dataclass(frozen=True)
-        class Result:
-            token: Optional['Lexer.Token']
-            state: 'Lexer.State'
 
-        @abstractmethod
-        def apply(self, state: 'Lexer.State') -> 'Lexer.Rule.Result': ...
+Error = processor.Error
 
-    @dataclass(frozen=True)
-    class Position:
-        index: int
+RuleError = processor.RuleError[ResultValue, State]
 
-    @dataclass(frozen=True)
-    class Token:
-        rule_name: str
-        value: str
-        position: 'Lexer.Position'
+NestedRuleError = processor.NestedRuleError[ResultValue, State]
 
-        def __repr__(self) -> str:
-            return f'Token({self.rule_name},{self.value})'
+Rule = processor.Rule[ResultValue, State]
 
-    @dataclass(frozen=True)
-    class Result:
-        tokens: Sequence['Lexer.Token']
+And = processor.And[ResultValue, State]
 
-    def apply(self, input: str) -> 'Lexer.Result':
-        toks: MutableSequence[Lexer.Token] = []
-        state: Lexer.State = Lexer.State(input)
-        while not state.empty:
-            rule_results: Sequence['Lexer.Rule.Result'] = [result for result in [
-                rule.apply(state) for rule in self.rules] if result is not None]
-            if len(rule_results) > 1:
-                raise Lexer.AmbiguityError(state, rule_results)
-            elif not rule_results:
-                raise Lexer.Error(state)
-            rule_result = rule_results[0]
-            if rule_result.token is not None:
-                toks.append(rule_result.token)
-            state = rule_result.state
-        return Lexer.Result(toks)
+Or = processor.Or[ResultValue, State]
+
+ZeroOrMore = processor.ZeroOrMore[ResultValue, State]
+
+OneOrMore = processor.OneOrMore[ResultValue, State]
+
+ZeroOrOne = processor.ZeroOrOne[ResultValue, State]
+
+Result = processor.Result[ResultValue, State]
+
+ResultAndState = processor.ResultAndState[ResultValue, State]
+
+UntilEmpty = processor.UntilEmpty[ResultValue, State]
+
+
+@dataclass(frozen=True)
+class Token:
+    type: str
+    value: str
+
+
+@dataclass(frozen=True)
+class TokenStream:
+    tokens: Sequence[Token]
+    pos: int = 0
+
+    @cached_property
+    def empty(self) -> bool:
+        return self.pos >= len(self.tokens)
+
+    @cached_property
+    def head(self) -> Token:
+        assert not self.empty
+        return self.tokens[self.pos]
+
+    @cached_property
+    def tail(self) -> 'TokenStream':
+        return TokenStream(self.tokens, self.pos+1)
+
+
+_ROOT_RULE_NAME = '_root'
+_RULES_RULE_NAME = '_rules'
+
+
+@dataclass(frozen=True, init=False)
+class Lexer(processor.Processor[ResultValue, State]):
+    @staticmethod
+    def _flatten_result_value(result: Result) -> str:
+        value: str = ''
+        if result.value is not None:
+            value += result.value.value
+        for child in result.children:
+            value += Lexer._flatten_result_value(child)
+        return value
+
+    @staticmethod
+    def _token_from_result(result: Result):
+        assert result.rule.name == _RULES_RULE_NAME, result
+        assert len(result.children) == 1, result
+        rule_result: Result = result.children[0]
+        assert rule_result.rule.name is not None, rule_result
+        return Token(rule_result.rule.name, Lexer._flatten_result_value(rule_result))
+
+    @staticmethod
+    def _token_stream_from_result(result: Result) -> TokenStream:
+        assert result.rule.name == _ROOT_RULE_NAME, result
+        return TokenStream([Lexer._token_from_result(child) for child in result.children])
+
+    def __init__(self, rules: Sequence[Rule]):
+        assert all([rule.name is not None for rule in rules])
+        super().__init__(_ROOT_RULE_NAME, [
+            UntilEmpty(_ROOT_RULE_NAME, Or(_RULES_RULE_NAME, rules))])
+
+    def apply(self, input: str) -> TokenStream:
+        return self._token_stream_from_result(self._apply(State(input)))
---
+++ core/lexer_test.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from typing import Sequence
+from unittest import TestCase
+
+from . import lexer
+
+
+@dataclass(frozen=True)
+class StartsWith(lexer.Rule):
+    value: str
+
+    def apply(self, state: lexer.State) -> lexer.ResultAndState:
+        if state.value.startswith(self.value):
+            result_value: lexer.ResultValue = lexer.ResultValue(self.value)
+            return lexer.ResultAndState(lexer.Result(self, result_value, []), state.after(result_value))
+        raise lexer.RuleError(self, state)
+
+
+class StateTest(TestCase):
+    def test_empty(self):
+        self.assertTrue(lexer.State('').empty)
+        self.assertFalse(lexer.State('foo').empty)
+
+    def test_value(self):
+        self.assertEqual('foo', lexer.State('foo').value)
+        self.assertEqual('bar', lexer.State('foobar', 3).value)
+
+    def test_with_pos(self):
+        self.assertEqual(10, lexer.State('').with_pos(10).pos)
+
+    def test_after(self):
+        self.assertEqual(lexer.State('foobar', 3), lexer.State(
+            'foobar').after(lexer.ResultValue('foo')))
+        with self.assertRaises(ValueError):
+            lexer.State('foobar').after(lexer.ResultValue('baz'))
+
+
+class TokenStreamTest(TestCase):
+    def test_empty(self):
+        self.assertTrue(lexer.TokenStream([]).empty)
+        self.assertFalse(lexer.TokenStream([lexer.Token('a', 'b')]).empty)
+        self.assertTrue(lexer.TokenStream([lexer.Token('a', 'b')], 1).empty)
+
+    def test_head(self):
+        self.assertEqual(lexer.Token('a', 'b'), lexer.TokenStream(
+            [lexer.Token('a', 'b')]).head)
+
+    def test_tail(self):
+        toks: Sequence[lexer.Token] = [
+            lexer.Token('a', 'b'), lexer.Token('c', 'd')]
+        self.assertEqual(
+            lexer.TokenStream(toks, 1),
+            lexer.TokenStream(toks).tail
+        )
+
+
+class LexerTest(TestCase):
+    def test_lex(self):
+        input: str
+        output: Sequence[lexer.Token]
+        for input, output in [
+            ('a', [lexer.Token('a', 'a')]),
+            ('c', [lexer.Token('b', 'c')]),
+            ('cc', [lexer.Token('b', 'cc')]),
+            ('acc', [lexer.Token('a', 'a'), lexer.Token('b', 'cc')]),
+        ]:
+            self.assertEqual(
+                lexer.Lexer([
+                    StartsWith('a', 'a'),
+                    lexer.OneOrMore('b', StartsWith(None, 'c')),
+                ]).apply(input),
+                lexer.TokenStream(output)
+            )
