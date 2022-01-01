3558a4f1de69b8591cfb27670190efb7d49d591f
Dave Carr <davecarr1024@gmail.com>
2022-01-01 12:32:13 -0500

    types_test

--- pysh/types_.py
+++ pysh/types_.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod, abstractproperty
-from typing import Mapping, Sequence
+from typing import Generic, Iterable, Mapping, MutableMapping, Optional, TypeVar
 
 from pysh import errors
 
@@ -13,24 +13,27 @@ class Type(ABC):
     @abstractproperty
     def name(self) -> str: ...
 
-    @abstractproperty
-    def member_types(self) -> Mapping[str, 'Type']: ...
-
     @abstractmethod
     def check_assignable(self, type: 'Type') -> None: ...
 
 
+class Callable(Type):
+    @abstractproperty
+    def signatures(self) -> 'Signatures': ...
+
+
+class Compound(Type):
+    @abstractproperty
+    def member_types(self) -> Mapping[str, 'Type']: ...
+
+
 @dataclass(frozen=True)
-class BuiltinType(Type):
+class Builtin(Type):
     _name: str
 
     @property
     def name(self) -> str:
         return self._name
-
-    @property
-    def member_types(self) -> Mapping[str, Type]:
-        return {}
 
     def check_assignable(self, type: 'Type') -> None:
         if type != self:
@@ -38,30 +41,42 @@ class BuiltinType(Type):
 
 
 @dataclass(frozen=True)
+class Arg:
+    type: Type
+
+
+class Args(list[Arg]):
+    def __init__(self, args: Iterable[Arg]):
+        super().__init__(args)
+
+
+@dataclass(frozen=True)
 class Param:
     name: str
     type: Type
 
-    def check_assignable(self, arg_type: Type) -> None:
+    def check_assignable(self, arg: Arg) -> None:
         try:
-            self.type.check_assignable(arg_type)
+            self.type.check_assignable(arg.type)
         except errors.Error as error:
-            raise Error(f'{self} cannot assign type {arg_type}: {error}')
+            raise Error(f'param {self} cannot assign arg {arg}: {error}')
 
 
-@dataclass(frozen=True)
-class Params:
-    params: Sequence[Param]
+class Params(list[Param]):
+    def __init__(self, params: Iterable[Param]):
+        super().__init__(params)
 
-    def check_assignable(self, arg_types: Sequence[Type]) -> None:
-        if len(self.params) != len(arg_types):
+    def check_assignable(self, args: Args) -> None:
+        if len(self) != len(args):
             raise Error(
-                f'{self} expected {len(self.params)} args but got {len(arg_types)}')
-        for param, arg_type in zip(self.params, arg_types):
-            param.check_assignable(arg_type)
+                f'{self} expected {len(self)} args but got {len(args)}')
+        for param, arg in zip(self, args):
+            param.check_assignable(arg)
 
     def without_first_param(self) -> 'Params':
-        return Params(self.params[1:])
+        if len(self) == 0:
+            raise Error(f'{self} unable to remove first param: empty')
+        return Params(self[1:])
 
 
 @dataclass(frozen=True)
@@ -69,11 +84,157 @@ class Signature:
     params: Params
     return_type: Type
 
-    def check_args_assignable(self, arg_types: Sequence[Type]) -> None:
-        self.params.check_assignable(arg_types)
+    def check_args_assignable(self, args: Args) -> None:
+        self.params.check_assignable(args)
 
-    def check_return_val_assignable(self, return_type: Type) -> None:
+    def check_return_assignable(self, return_type: Type) -> None:
         self.return_type.check_assignable(return_type)
 
     def without_first_param(self) -> 'Signature':
         return Signature(self.params.without_first_param(), self.return_type)
+
+
+class Signatures(list[Signature]):
+    def __init__(self, signatures: Iterable[Signature]):
+        super().__init__(signatures)
+
+    def check_args_assignable(self, args: Args) -> None:
+        if len(self) == 0:
+            raise Error(f'empty signature set')
+        elif len(self) == 1:
+            self[0].check_args_assignable(args)
+        else:
+            errors = list[Error]()
+            for signature in self:
+                try:
+                    signature.check_args_assignable(args)
+                    return
+                except Error as error:
+                    errors.append(error)
+            raise Error(f'all signatures failed to match args: {errors}')
+
+    def check_return_assignable(self, return_type: Type) -> None:
+        if len(self) == 0:
+            raise Error(f'empty signature set')
+        elif len(self) == 1:
+            self[0].check_return_assignable(return_type)
+        else:
+            errors = list[Error]()
+            for signature in self:
+                try:
+                    signature.check_return_assignable(return_type)
+                    return
+                except Error as error:
+                    errors.append(error)
+            raise Error(f'all signatures failed to match args: {errors}')
+
+    def without_first_param(self) -> 'Signatures':
+        return Signatures(signature.without_first_param() for signature in self)
+
+
+class Val(ABC):
+    @abstractproperty
+    def type(self) -> Type: ...
+
+
+_ValType = TypeVar('_ValType', bound=Val)
+
+
+@dataclass
+class Var(Generic[_ValType]):
+    _type: Type
+    _val: _ValType
+
+    def __post_init__(self):
+        try:
+            self._type.check_assignable(self._val.type)
+        except Error as error:
+            raise Error(f'{self} has incompatible val: {error}')
+
+    @property
+    def type(self) -> Type:
+        return self._type
+
+    @property
+    def val(self) -> _ValType:
+        return self._val
+
+    @val.setter
+    def val(self, val: _ValType) -> None:
+        raise Error(f'unable to set {self} with {val}: immutable')
+
+    def check_assignable(self, val: _ValType) -> None:
+        try:
+            self._type.check_assignable(val.type)
+        except Error as error:
+            raise Error(f'{self} unable to be set with val {val}: {error}')
+
+    @staticmethod
+    def for_val(val: _ValType) -> 'Var[_ValType]':
+        return Var[_ValType](val.type, val)
+
+
+@dataclass
+class MutableVar(Var[_ValType]):
+    @property
+    def val(self) -> _ValType:
+        return self._val
+
+    @val.setter
+    def val(self, val: _ValType) -> None:
+        self.check_assignable(val)
+        self._val = val
+
+    @staticmethod
+    def for_val(val: _ValType) -> 'MutableVar[_ValType]':
+        return MutableVar[_ValType](val.type, val)
+
+
+@dataclass(frozen=True)
+class Scope(Generic[_ValType]):
+    _vars: MutableMapping[str, Var[_ValType]]
+    parent: Optional['Scope[_ValType]'] = None
+
+    def __contains__(self, name: str) -> bool:
+        return name in self._vars or (self.parent is not None and name in self.parent)
+
+    def __getitem__(self, name: str) -> _ValType:
+        if name in self._vars:
+            return self._vars[name].val
+        elif self.parent is not None:
+            return self.parent[name]
+        else:
+            raise Error(f'unknown var {name}')
+
+    @property
+    def vals(self) -> Mapping[str, _ValType]:
+        return {name: var.val for name, var in self._vars.items()}
+
+    def all_vals(self) -> Mapping[str, _ValType]:
+        vals = dict[str, _ValType]()
+        if self.parent is not None:
+            vals.update(self.parent.all_vals())
+        vals.update(self.vals)
+        return vals
+
+    def all_types(self) -> Mapping[str, Type]:
+        return {name: val.type for name, val in self.all_vals().items()}
+
+
+@dataclass(frozen=True)
+class MutableScope(Scope[_ValType]):
+    def __setitem__(self, name: str, val: _ValType) -> None:
+        if name in self._vars:
+            try:
+                self._vars[name].val = val
+            except Error as error:
+                raise Error(f'unable to set {name} to val {val}: {error}')
+        elif isinstance(self.parent, MutableScope):
+            self.parent[name] = val
+        else:
+            raise Error(f'unknown var {name}')
+
+    def decl(self, name: str, var: Var[_ValType]) -> None:
+        if name in self._vars:
+            raise Error(f'duplicate var {name}')
+        self._vars[name] = var
--- pysh/types_test.py
+++ pysh/types_test.py
@@ -1,47 +1,243 @@
+from dataclasses import dataclass
+from typing import Optional, Sequence
 from pysh import types_
 
 from unittest import TestCase
 
-int_ = types_.BuiltinType('int')
-str_ = types_.BuiltinType('str')
+int_type = types_.Builtin('int')
+int_arg = types_.Arg(int_type)
+
+
+def int_param(name: str = 'a') -> types_.Param:
+    return types_.Param(name, int_type)
+
+
+str_type = types_.Builtin('str')
+str_arg = types_.Arg(str_type)
+
+
+def str_param(name: str = 's') -> types_.Param:
+    return types_.Param(name, str_type)
 
 
 class BuiltinTypeTest(TestCase):
     def test_name(self):
-        self.assertEqual(int_.name, 'int')
+        self.assertEqual(int_type.name, 'int')
 
     def test_check_assignable(self):
-        int_.check_assignable(int_)
+        int_type.check_assignable(int_type)
         with self.assertRaises(types_.Error):
-            int_.check_assignable(str_)
+            int_type.check_assignable(str_type)
 
 
 class ParamTest(TestCase):
     def test_check_assignable(self):
-        types_.Param('a', int_).check_assignable(int_)
+        int_param().check_assignable(int_arg)
         with self.assertRaises(types_.Error):
-            types_.Param('a', int_).check_assignable(str_)
+            int_param().check_assignable(str_arg)
 
 
 class ParamsTest(TestCase):
     def test_check_assignable(self):
-        params = types_.Params([types_.Param('a', int_)])
-        params.check_assignable([int_])
+        params = types_.Params([int_param()])
+        params.check_assignable(types_.Args([int_arg]))
         with self.assertRaises(types_.Error):
-            params.check_assignable([])
+            params.check_assignable(types_.Args([]))
         with self.assertRaises(types_.Error):
-            params.check_assignable([str_])
+            params.check_assignable(types_.Args([str_arg]))
+
+    def test_without_first_param(self):
+        self.assertEqual(types_.Params([int_param()]).without_first_param(),
+                         types_.Params([]))
+        with self.assertRaises(types_.Error):
+            types_.Params([]).without_first_param()
 
 
 class SignatureTest(TestCase):
     def test_check_assignable(self):
         signature = types_.Signature(types_.Params(
-            [types_.Param('a', int_), types_.Param('b', int_)]), int_)
-        signature.check_args_assignable([int_,int_])
+            [int_param('a'), int_param('b')]), int_type)
+        signature.check_args_assignable(
+            types_.Args([int_arg, int_arg]))
         with self.assertRaises(types_.Error):
-            signature.check_args_assignable([])
+            signature.check_args_assignable(types_.Args([]))
         with self.assertRaises(types_.Error):
-            signature.check_args_assignable([int_,str_])
-        signature.check_return_val_assignable(int_)
+            signature.check_args_assignable(
+                types_.Args([int_arg, str_arg]))
+        signature.check_return_assignable(int_type)
         with self.assertRaises(types_.Error):
-            signature.check_return_val_assignable(str_)
+            signature.check_return_assignable(str_type)
+
+    def test_without_first_param(self):
+        self.assertEqual(types_.Signature(types_.Params([int_param()]), int_type).without_first_param(),
+                         types_.Signature(types_.Params([]), int_type))
+        with self.assertRaises(types_.Error):
+            types_.Signature(types_.Params([]), int_type).without_first_param()
+
+
+class SignaturesTest(TestCase):
+    def test_check_assignable(self):
+        signatures = types_.Signatures([
+            types_.Signature(
+                types_.Params([
+                    int_param(),
+                ]),
+                int_type
+            ),
+            types_.Signature(
+                types_.Params([
+                    str_param(),
+                ]),
+                int_type
+            ),
+        ])
+        signatures.check_args_assignable(types_.Args([int_arg]))
+        signatures.check_args_assignable(types_.Args([str_arg]))
+        with self.assertRaises(types_.Error):
+            signatures.check_args_assignable(types_.Args([]))
+        with self.assertRaises(types_.Error):
+            signatures.check_args_assignable(types_.Args([int_arg, str_arg]))
+        signatures.check_return_assignable(int_type)
+        with self.assertRaises(types_.Error):
+            signatures.check_return_assignable(str_type)
+
+    def test_without_first_param(self):
+        self.assertEqual(types_.Signatures([types_.Signature(types_.Params([int_param()]), int_type)]).without_first_param(),
+                         types_.Signatures([types_.Signature(types_.Params([]), int_type)]))
+        with self.assertRaises(types_.Error):
+            types_.Signatures(
+                [types_.Signature(types_.Params([]), int_type)]).without_first_param()
+
+
+@dataclass(frozen=True)
+class Val(types_.Val):
+    _type: types_.Type
+    name: str = 'v'
+
+    @property
+    def type(self) -> types_.Type:
+        return self._type
+
+
+def val(type: types_.Type, name: str) -> Val:
+    return Val(type, name)
+
+
+def int_val(name: str = 'i') -> Val:
+    return val(int_type, name)
+
+
+def str_val(name: str = 's') -> Val:
+    return val(str_type, name)
+
+
+Var = types_.Var[Val]
+
+
+def int_var(name: str = 'i', type: type[Var] = Var) -> Var:
+    return type(int_type, int_val(name))
+
+
+def str_var(name: str = 's', type: type[Var] = Var) -> Var:
+    return type(str_type, str_val(name))
+
+
+class VarTest(TestCase):
+    def test_validate(self):
+        int_var()
+        with self.assertRaises(types_.Error):
+            types_.Var(int_type, Val(str_type))
+
+    def test_type(self):
+        self.assertEqual(int_var().type, int_type)
+
+    def test_val(self):
+        self.assertEqual(int_var().val, int_val())
+
+    def test_check_assignable(self):
+        int_var().check_assignable(int_val())
+        with self.assertRaises(types_.Error):
+            int_var().check_assignable(str_val())
+
+    def test_for_val(self):
+        self.assertEqual(Var.for_val(int_val()), int_var())
+
+    def test_set(self):
+        with self.assertRaises(types_.Error):
+            int_var().val = int_val()
+
+
+MutableVar = types_.MutableVar[Val]
+
+
+class MutableVarTest(TestCase):
+    def test_set(self):
+        i = int_var('i', MutableVar)
+        self.assertEqual(i.val, int_val('i'))
+        i.val = int_val('j')
+        self.assertEqual(i.val, int_val('j'))
+
+
+Scope = types_.Scope[Val]
+
+
+def scope(vals: Sequence[Val], parent: Optional[Scope] = None) -> Scope:
+    return Scope({val.name: Var.for_val(val) for val in vals}, parent)
+
+
+class ScopeTest(TestCase):
+    def test_contains(self):
+        s = scope([int_val('a')], scope([int_val('b')]))
+        self.assertTrue('a' in s)
+        self.assertTrue('b' in s)
+        self.assertFalse('c' in s)
+
+    def test_getitem(self):
+        s = scope([int_val('a')], scope([int_val('b')]))
+        self.assertEqual(int_val('a'), s['a'])
+        self.assertEqual(int_val('b'), s['b'])
+        with self.assertRaises(types_.Error):
+            s['c']
+
+    def test_vals(self):
+        self.assertEqual(
+            scope([int_val('a')], scope([int_val('b')])).vals,
+            {'a': int_val('a')}
+        )
+
+    def test_all_vals(self):
+        self.assertEqual(
+            scope([int_val('a')], scope([int_val('b')])).all_vals(),
+            {'a': int_val('a'), 'b': int_val('b')}
+        )
+
+    def test_all_types(self):
+        self.assertEqual(
+            scope([int_val('a')], scope([str_val('b')])).all_types(),
+            {'a': int_type, 'b': str_type}
+        )
+
+
+MutableScope = types_.MutableScope[Val]
+
+
+def mutable_scope(vals: Sequence[Val], parent: Optional[Scope] = None) -> MutableScope:
+    return MutableScope({val.name: MutableVar.for_val(val) for val in vals}, parent)
+
+
+class MutableScopeTest(TestCase):
+    def test_setitem(self):
+        s = mutable_scope([int_val('a')], mutable_scope([int_val('b')]))
+        s['a'] = int_val('c')
+        self.assertEqual(s['a'], int_val('c'))
+        s['b'] = int_val('d')
+        self.assertEqual(s['b'], int_val('d'))
+        with self.assertRaises(types_.Error):
+            s['e'] = int_val('f')
+
+    def test_decl(self):
+        s = mutable_scope([int_val('a')], mutable_scope([int_val('b')]))
+        s.decl('b', int_var('c'))
+        self.assertEqual(s['b'], int_val('c'))
+        with self.assertRaises(types_.Error):
+            s.decl('a', int_var('d'))
